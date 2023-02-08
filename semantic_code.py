c3c3c19d50d166f8abd82781df0dc95a8c4b49bc
Brownsey <brownsey666@gmail.com>
2023-02-08 17:15:11 +0000

    scores method from hugging face

--- semantic_code.py
+++ semantic_code.py
@@ -1,13 +1,49 @@
 import pandas as pd
+import torch
+from transformers import AutoTokenizer, AutoModel
+import torch.nn.functional as F
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+
 txt_data = pd.read_csv('data/output.txt', sep='\t', header= None, names = ["text"])
-text = txt_data['text'].tolist()
+sentences = txt_data['text'].tolist()
 
-from sentence_transformers import SentenceTransformer
+model_ckpt = "sentence-transformers/all-MiniLM-L6-v2"
+tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
+model = AutoModel.from_pretrained(model_ckpt)
 
-model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-embeddings = model.encode(text)
-print(embeddings)
+encoded_input = tokenizer(sentences, padding=True, truncation=True, return_tensors="pt")
+
+with torch.no_grad():
+    model_output = model(**encoded_input)
+    
+    
+token_embeddings = model_output.last_hidden_state
+print(f"Token embeddings shape: {token_embeddings.size()}")
+
+def mean_pooling(model_output, attention_mask):
+    token_embeddings = model_output.last_hidden_state
+    input_mask_expanded = (
+        attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    )
+    return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(
+        input_mask_expanded.sum(1), min=1e-9
+    )
 
 
-#pooling for whole sentence, mean_pooling is average
-#cosine similarity
+sentence_embeddings = mean_pooling(model_output, encoded_input["attention_mask"])
+# Normalize the embeddings
+sentence_embeddings = F.normalize(sentence_embeddings, p=2, dim=1)
+print(f"Sentence embeddings shape: {sentence_embeddings.size()}")
+
+
+sentence_embeddings = sentence_embeddings.detach().numpy()
+
+scores = np.zeros((sentence_embeddings.shape[0], sentence_embeddings.shape[0]))
+
+for idx in range(sentence_embeddings.shape[0]):
+    scores[idx, :] = cosine_similarity([sentence_embeddings[idx]], sentence_embeddings)[0]
+
+scores
+
+#Define the threshold and get the indices of the sentences that are similar
